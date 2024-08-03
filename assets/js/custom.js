3e4ed07920eea140544607c079f375ea1d01aed4
mhmdrzax <mohammadreza.ahadiyan@gmail.com>
2024-08-03 12:31:58 +0330

    fix links

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,14 @@
 
 const offcanvasRight = document.getElementById('offcanvasRight')
 const headerElement = document.querySelector(".header-hero")
+
+//add class when sidebar opens
 offcanvasRight.addEventListener('show.bs.offcanvas', event => {
     headerElement.classList.add("blur-5")
+})
+
+//remove class when sidebar closes
+
+offcanvasRight.addEventListener('hide.bs.offcanvas', event => {
+    headerElement.classList.remove("blur-5")
 })
