70bfe5a3f57bdb1a1c7ab5a5447eee597b6262b7
mhmdrzax <mohammadreza.ahadiyan@gmail.com>
2024-08-03 11:51:58 +0330

    add comment to js file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+//Handle styles by bootstrap events
+
 const offcanvasRight = document.getElementById('offcanvasRight')
 const headerElement = document.querySelector(".header-hero")
 offcanvasRight.addEventListener('show.bs.offcanvas', event => {
