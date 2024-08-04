8b43b30b94ab2b2a39c96f9a18399933504c418d
mhmdrzax <mohammadreza.ahadiyan@gmail.com>
2024-08-04 12:26:14 +0330

    create nested canvas menu and handle designs

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,19 @@
 //Handle styles by bootstrap events
 
-const offcanvasRight = document.getElementById('offcanvasRight')
-const headerElement = document.querySelector(".header-hero")
+const offcanvasRight = document.getElementsByClassName("offcanvas");
+const headerElement = document.querySelector(".header-hero");
 
-//add class when sidebar opens
-offcanvasRight.addEventListener('show.bs.offcanvas', event => {
-    headerElement.classList.add("blur-5")
-})
+const iterableOffcanvasRight = Array.from(offcanvasRight);
 
-//remove class when sidebar closes
+iterableOffcanvasRight.forEach((item) => {
+  //add class when sidebar opens
+  item.addEventListener("show.bs.offcanvas", (event) => {
+    headerElement.classList.add("blur-5");
+  });
 
-offcanvasRight.addEventListener('hide.bs.offcanvas', event => {
-    headerElement.classList.remove("blur-5")
-})
+  //remove class when sidebar closes
+
+  item.addEventListener("hide.bs.offcanvas", (event) => {
+    headerElement.classList.remove("blur-5");
+  });
+});
