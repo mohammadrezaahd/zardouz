8f777fdb9906687e092c44c208f8c84f8a6f678f
mhmdrzax <mohammadreza.ahadiyan@gmail.com>
2024-08-16 02:32:07 +0330

    single product page start

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,6 +4,7 @@ const mainContainer = document.querySelector(".main-container");
 const bagToastBtn = document.querySelector(".bag-toast-btn");
 const bagToastElement = document.querySelector(".bagToast");
 const cartCount = document.querySelector(".cart-count");
+const singleProductSlider = document.querySelector(".single-product-slider");
 const offcanvasRightElements = makeObjectsIterable(
   document.getElementsByClassName("offcanvas")
 );
@@ -95,6 +96,62 @@ shoppingBagElements.forEach((item) => {
 
 bagToastBtn.addEventListener("click", () => {
   bagToastElement.classList.add("toast-hidden");
+});
+
+//single product swiper
+document.addEventListener("DOMContentLoaded", () => {
+  var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    on: {
+      reachEnd: () => {
+        let startY;
+        let endY;
+        const touchStartHandler = (e) => {
+          startY = e.changedTouches[0].clientY;
+          singleProductSlider.addEventListener("touchend", touchEndHandler);
+        };
+        const touchEndHandler = (e) => {
+          endY = startY = e.changedTouches[0].clientY;
+          console.log('startY:', startY)
+          console.log('endY:', endY)
+          if(endY > startY){
+            alert("ASdasd")
+          }
+          // if(endY > startY){
+          //   window.scrollTo({
+          //     top: window.scrollY + window.innerHeight,
+          //     behavior: "smooth",
+          //   });
+          // } else {
+          //   singleProductSlider.removeEventListener("touchstart", touchStartHandler);
+          //   singleProductSlider.removeEventListener("touchend", touchEndHandler);
+          // }
+        };
+
+        const wheelHandler = (e) => {
+          if (e.deltaY > 0) {
+            window.scrollTo({
+              top: window.scrollY + window.innerHeight,
+              behavior: "smooth",
+            });
+          } else {
+            singleProductSlider.removeEventListener("wheel", wheelHandler);
+          }
+        };
+        setTimeout(() => {
+          singleProductSlider.addEventListener("wheel", wheelHandler);
+          singleProductSlider.addEventListener("touchstart", touchStartHandler);
+        }, 400);
+      },
+    },
+  });
 });
 
 //Convert like arrays to array
