3f9f1342a2dc262e1377eaa0bd257c5f4757a10d
mhmdrzax <mohammadreza.ahadiyan@gmail.com>
2024-08-17 23:11:39 +0330

    cart(shopping bag) - full responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -111,14 +111,20 @@ document.addEventListener("DOMContentLoaded", () => {
         };
         const touchEndHandler = (e) => {
           endY = e.changedTouches[0].clientY;
-          if(startY > endY){
+          if (startY > endY) {
             window.scrollTo({
-              top: window.scrollY + (window.innerHeight),
+              top: window.scrollY + window.innerHeight,
               behavior: "smooth",
             });
           } else {
-            singleProductSlider.removeEventListener("touchstart", touchStartHandler);
-            singleProductSlider.removeEventListener("touchend", touchEndHandler);
+            singleProductSlider.removeEventListener(
+              "touchstart",
+              touchStartHandler
+            );
+            singleProductSlider.removeEventListener(
+              "touchend",
+              touchEndHandler
+            );
           }
         };
 
@@ -140,6 +146,23 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 });
+
+//product count controller
+function increaseValue(button, limit) {
+  const numberInput = button.parentElement.querySelector(".number");
+  var value = parseInt(numberInput.innerHTML, 10);
+  if (isNaN(value)) value = 0;
+  if (limit && value >= limit) return;
+  numberInput.innerHTML = value + 1;
+}
+
+function decreaseValue(button) {
+  const numberInput = button.parentElement.querySelector(".number");
+  var value = parseInt(numberInput.innerHTML, 10);
+  if (isNaN(value)) value = 0;
+  if (value < 1) return;
+  numberInput.innerHTML = value - 1;
+}
 
 //Convert like arrays to array
 function makeObjectsIterable(obj) {
